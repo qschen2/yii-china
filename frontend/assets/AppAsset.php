e41b56b96ac28bf63808d991014a0f1e37d14d82
qschen12 <2399815778@qq.com>
2016-07-15 21:35:55 +0800

    静态资源配置

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,11 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'statics/css/site.css',
+        'statics/css/test.css',
     ];
     public $js = [
+        'statics/js/test.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
