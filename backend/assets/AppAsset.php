cf1ffff91764836836b49604a746f1de939bc980
qschen12 <2399815778@qq.com>
2016-07-17 15:44:05 +0800

    后台主题模块的更换

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,12 +12,27 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'statics/css/bootstrap.min.css',
+        'statics/css/bootstrap-responsive.min.css',
+        'statics/css/font-awesome.min.css',
+        'statics/css/style-metro.css',
+        'statics/css/style.css',
+        'statics/css/style-responsive.css',
+        'statics/css/default.css',
+        'statics/css/uniform.default.css',
     ];
     public $js = [
+        'statics/js/jquery-1.10.1.min.js',
+        'statics/js/jquery-migrate-1.2.1.min.js',
+        'statics/js/jquery-ui-1.10.1.custom.min.js',
+        'statics/js/bootstrap.min.js',
+        'statics/js/jquery.slimscroll.min.js',
+        'statics/js/jquery.blockui.min.js',
+        'statics/js/jquery.cookie.min.js',
+        'statics/js/jquery.uniform.min.js',
+        'statics/js/app.js',
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        
     ];
 }
